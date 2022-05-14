a61c75668da7c728552c7a29d77ec4a8a0b9cafe
Henry HENG Hian Wee <deezombiedude612@hotmail.com>
2022-05-14 19:29:41 +0800

    Update Task04.java

--- 202204ITS42004/Practical03/Task04.java
+++ 202204ITS42004/Practical03/Task04.java
@@ -1,17 +1,11 @@
 package Practical03;
 
-/**
- * Write a program that displays the first 50 prime numbers in five lines, each of which contains 10 numbers.
- * An integer greater than 1 is prime if its only positive divisor is 1 or itself.
- * For example, 2, 3, 5 and 7 are prime numbers, but 4, 6, 8 and 9 are not.
- */
-
 public class Task04 {
 	public static void main(String[] args) {
 		// index - count the number of printed prime numbers
 		// num - testing number(s) starting from 2 to see if they are prime
 		for (int num = 2, index = 0; index < 50; num++) {
-			boolean isPrime = true;
+			boolean isPrime = true;     // flag to say if num in this iteration is prime or not
 
 			// check if num is prime
 			for (int j = 2; j <= num / 2; j++) {
@@ -25,7 +19,7 @@ public class Task04 {
 			// if number is prime, print number
 			if (isPrime) {
 				System.out.print(num + "\t");
-				index++;
+				index++;    // increment index as soon as a prime number has been printed
 
 				// after 10 numbers have been printed in one line, add new line escape character
 				if (index % 10 == 0)
