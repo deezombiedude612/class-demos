3f6b24a8723b6802f5b456631a8aff46cdda321c
Henry HENG Hian Wee <deezombiedude612@hotmail.com>
2022-10-16 00:34:42 +0800

    Added OOP Practical 5 Challenge Task 1 & 3

---
+++ 202208ITS42004/Practical05/ChallengeTask01.java
@@ -0,0 +1,83 @@
+package Practical05;
+
+public class ChallengeTask01 {
+	public static int[][] initBoard() {
+		int[][] board = new int[3][3];
+
+		for (int i = 0; i < board.length; ++i) {
+			for (int j = 0; j < board[i].length; ++j) {
+				board[i][j] = (int)(Math.random() * 2);
+				System.out.print(board[i][j]);  // print each cell as they are made up
+			}
+			System.out.println();   // print new line character after each row has been fully printed
+		}
+
+		return board;
+	}
+
+	// check rows (horizontally)
+	public static void rows(int[][] board) {
+		for (int i = 0; i < board.length; ++i) {
+			boolean flag = true;    // flag for if whole row contains same element
+			for (int j = 1; j < board[i].length; ++j) {
+				// if any element along row does not match first element
+				if (board[i][0] != board[i][j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) System.out.println("All " + board[i][0] + "'s on row " + i);
+		}
+	}
+
+	// check columns
+	public static void columns(int[][] board) {
+		for (int i = 0; i < board.length; ++i) {
+			boolean flag = true;    // flag for if whole column contains same element
+			for (int j = 1; j < board[i].length; ++j) {
+				// if any element along column does not match first element
+				if (board[0][i] != board[j][i]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) System.out.println("All " + board[0][i] + "'s on column " + i);
+		}
+	}
+
+	/**
+	 * [0,0]    [0,1]   [0,2]
+	 * [1,0]    [1,1]   [1,2]
+	 * [2,0]    [2,1]   [2,2]
+	 */
+
+	// check major diagonal (diagonal from top left to bottom right of square matrix)
+	public static boolean majorDiagonal(int[][] board) {
+		for (int i = 1; i < board.length; ++i) {
+			if (board[0][0] != board[i][i])  // if any element along major diagonal does not match first element
+				return false;
+		}
+		return true;
+	}
+
+	// check sub-diagonal (diagonal from top right to bottom left of square matrix)
+	public static boolean subDiagonal(int[][] board) {
+		for (int i = 1; i < board.length; ++i) {
+			// if any element along sub-diagonal does not match first element
+			if (board[0][board.length - 1] != board[i][board[i].length - i - 1])
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("TicTacToe board:");
+		int[][] board = initBoard();
+
+		System.out.println("\nResult(s):");
+		rows(board);
+		columns(board);
+		if (majorDiagonal(board)) System.out.println("All " + board[0][0] + "'s on major diagonal");
+		if (subDiagonal(board)) System.out.println("All " + board[0][0] + "'s on sub-diagonal");
+	}
+}
--- 202208ITS42004/Practical05/ChallengeTask03.java
+++ 202208ITS42004/Practical05/ChallengeTask03.java
@@ -3,8 +3,95 @@ package Practical05;
 import java.util.Scanner;
 
 public class ChallengeTask03 {
+	private final static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
+		long creditCardNo = getCreditCardNo();
+
+		System.out.println(creditCardNo + " is " + (isValid(creditCardNo) ? "valid" : "invalid"));
 	}
+
+	public static long getCreditCardNo() {
+		long creditCardNo = 0;
+
+		do {
+			System.out.print("Enter credit card number as a long integer >> ");
+			creditCardNo = input.nextLong();
+
+			if (!isValidLength(creditCardNo))
+				System.out.println("Invalid credit card number! Try again..");
+		} while (!isValidLength(creditCardNo));
+
+		return creditCardNo;
+	}
+
+	// Returns true if card is between 13 and 16 digits long
+	public static boolean isValidLength(long number) {
+		return ((int) (number / 1e13) > 0 && (int) (number / 1e16) == 0);
+	}
+
+	public static int getCardNumLength(long number) {
+		return String.valueOf(number).length();
+	}
+
+	// Returns true if the card number is valid
+	public static boolean isValid(long number) {
+		System.out.println("Sum of all even-placed single-digit numbers: " + sumOfDoubleEvenPlace(number));
+		System.out.println("Sum of all odd-placed digit numbers: " + sumOfOddPlace(number));
+
+		int sum = sumOfDoubleEvenPlace(number) + sumOfOddPlace(number);
+
+		return sum % 10 == 0;
+	}
+
+	// Get the result from Step 2
+	public static int sumOfDoubleEvenPlace(long number) {
+		int sum = 0;
+		do {
+			if (getCardNumLength(number) % 2 != 0) {
+				int digit = (int) (number % 10);
+
+//				int digitTwice = digit * 2;
+//				System.out.println(digit + " -> " + digitTwice);
+//
+//				if (digitTwice >= 10) sum += (digitTwice / 10) + (digitTwice % 10);
+//				else sum += digitTwice;
+
+				sum += getDigit(digit * 2);
+			}
+
+			number /= 10;
+		} while (number > 0);
+
+		return sum;
+	}
+
+	// Return this number of it is a single digit, otherwise, return the sum of the two digits
+	public static int getDigit(int number) {
+		return (number >= 10) ? (number / 10) + (number % 10) : number;
+	}
+
+	// Return sum of odd-place digits in number
+	public static int sumOfOddPlace(long number) {
+		int sum = 0;
+		do {
+			if (getCardNumLength(number) % 2 == 0) {
+				sum += (int) (number % 10);
+			}
+
+			number /= 10;
+		} while (number > 0);
+
+		return sum;
+	}
+
+	/*
+	 * STEPS:
+	 * 1. Double every second digit from right to left. If doubling of a digit results in a two-digit number, add up
+	 *    the two digits to get a single-digit number.
+	 * 2. Add all single-digit numbers from Step 1.
+	 * 3. Add all digits in the odd places from right to left in the card number.
+	 * 4. Sum the results from Step 2 and 3.
+	 * 5. If the result from Step 4 is divisible by 10, the card number is valid; otherwise, it is invalid.
+	 */
 }
